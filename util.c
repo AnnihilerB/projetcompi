864362a68595545cf2c46de7b677c47a2537ca1c
cfontenay <clement.fontenay@etu.u-bordeaux.fr>
2017-03-23 17:14:40 +0100

    tout marche, mais après comme mes fonctions utilisent des fonctions pas encore codé, et bien la fonction copier_bilfon, n'est pas encore sur de marcher

--- util.c
+++ util.c
@@ -36,7 +36,15 @@ BILFON creer_bilfon(LFON pfon)
 {
     BILFON b = bilfon_vide();
     b.debut = pfon;
-    b.fin = pfon;
+    LFON w = b.debut;
+    if (w != NULL)
+    {
+        while (w->SUIV != NULL)
+        {
+            w = w->SUIV;
+        }
+    }
+    b.fin = w;
     return b;
 }
 BILFON copier_bilfon(BILFON bfn)
@@ -44,11 +52,7 @@ BILFON copier_bilfon(BILFON bfn)
     BILFON b = bilfon_vide();
     if (bfn.debut != NULL)
     {
-        LFON w = bfn.debut;
-        while (w != NULL)
-        {
-            b = concatfn(b, creer_bilfon(w));
-        }
+        b = creer_bilfon(copier_fon(b.debut));
     }
     return b;
     
@@ -92,16 +96,22 @@ BILENV allvars(BILFON bfon)
 void ecrire_bilfon(BILFON bfn)
 {
     LFON w = bfn.debut;
-    while(w != NULL)
-    {
-        printf("ID: %s ( ", w->ID);
-        ecrire_bilenv(w->PARAM);
-        printf(" )\n variables locales: \n");
-        ecrire_bilenv(w->VARLOC);
-        printf("\n");
-        prefix(w->CORPS);
-        w = w->SUIV;
+    if (w == NULL)
+        printf("vide");
+    else
+    {        
+        while(w != NULL)
+        {
+            printf("ID: %s ( ", w->ID);
+            ecrire_bilenv(w->PARAM);
+            printf(" )\n variables locales: \n");
+            ecrire_bilenv(w->VARLOC);
+            printf("\n");
+            prefix(w->CORPS);
+            w = w->SUIV;
+        }
     }
+    printf("\n");
 }
 void ecrire_prog(BILENV argb,BILFON argbf,NOE argno)
 {
@@ -111,4 +121,35 @@ void ecrire_prog(BILENV argb,BILFON argbf,NOE argno)
     ecrire_bilfon(argbf);
     printf("argno: \n");
     prefix(argno);
+}
+int main(int argc, char** argv)
+{   
+    BILFON b = bilfon_vide();
+    printf("bilfon_vide: doit etre vide: ");
+    ecrire_bilfon(b);
+    LFON lfon = malloc(sizeof(struct cellfon));
+    lfon->ID = "test 1";
+    LFON lfon2 = malloc(sizeof(struct cellfon));
+    lfon2->ID = "test 2";
+    LFON lfon3 = malloc(sizeof(struct cellfon));
+    lfon3->ID = "test 3";
+    lfon->SUIV = lfon2->SUIV = lfon3->SUIV = NULL;
+    
+    printf("creer_bilfon: doit avoir un élement (test 1): \n");
+    b = creer_bilfon(lfon);
+    ecrire_bilfon(b);
+    lfon->SUIV = lfon2;
+    printf("creer_bilfon: doit avoir 2 élement (test 1 -> test 2): \n");
+    b = creer_bilfon(lfon);
+    ecrire_bilfon(b);
+    
+    printf("copier_bilfon: (en attente de copier_fon ");
+    BILFON b2 = copier_bilfon(b);
+    ecrire_bilfon(b2);
+    printf("debut: %p == %p  et fin: %p == %p\n", b.debut, b2.debut, b.fin, b2.fin);
+    
+    b2 = creer_bilfon(lfon3);
+    printf("concatfn:  doit avoir 3 élements (test1->test2->test3): \n");
+    b = concatfn(b,b2);
+    ecrire_bilfon(b);
 }
